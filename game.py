5f9140ef1238f6072e956d1a53bfa36ab3a84dfc
nuwandavek <vivekaithal44@gmail.com>
2025-01-28 10:40:31 -0800

    add report

--- game.py
+++ game.py
@@ -8,16 +8,18 @@ from agents import AVAILABLE_AGENTS
 from player import Player
 from tqdm import trange
 
+from report import GameLogger
+
 from dotenv import load_dotenv
 load_dotenv()
 
 class Game:
-    def __init__(self, players=List[Player], start_idx=0, verbose=False):
+    def __init__(self, players=List[Player], start_idx=0, logger=None):
         self.players = players
         self.winner_idx = -1
         self.active_players = [True] * len(self.players)
         self.current_player_idx = start_idx
-        self.verbose = verbose
+        self.logger = logger
         self.turns = 0
 
     @property
@@ -57,24 +59,21 @@ class Game:
     def start_turn(self):
         if self.current_player.state.in_tokyo:
             self.update_player_state(self.current_player, delta_vp=START_TOKYO_PTS)
-            if self.verbose:
-                print(f'{self.current_player} starts turn in Tokyo ({self.current_player.state})')
+            self.logger.log(f'{self.current_player} starts turn in Tokyo ({self.current_player.state})', category='event')
 
     def roll_n_dice(self, n):
         return [random.choice([DIESIDE.ATTACK, DIESIDE.HEAL, DIESIDE.ONE, DIESIDE.TWO, DIESIDE.THREE]) for _ in range(n)]
 
     def roll_dice(self):
         dice_results = self.roll_n_dice(DIE_COUNT)
-        if self.verbose:
-            print('\nStep 1: Rolling dice...')
-            print(f'roll 1: {[x.value for x in dice_results]}')
+        self.logger.log(f'\nStep 1: Rolling dice...', category='error')
+        self.logger.log(f'roll 1: {[x.value for x in dice_results]}', category='warning')
         
         for i in range(2):
             keep_mask = self.current_player.keep_dice(copy.deepcopy(dice_results), {player.name: (player.idx, player.state) for player in self.other_players}, roll_counter=i)
             dice_results = [dice_results[i] for i in range(DIE_COUNT) if keep_mask[i]] + self.roll_n_dice(DIE_COUNT - sum(keep_mask))
-            if self.verbose:
-                print(f'keep {i+1}: {keep_mask}')
-                print(f'roll {i+2}: {[x.value for x in dice_results]}')
+            self.logger.log(f'keep {i+1}: {keep_mask}', category='success')
+            self.logger.log(f'roll {i+2}: {[x.value for x in dice_results]}', category='warning')
 
         return dice_results
 
@@ -106,21 +105,21 @@ class Game:
                     self.update_player_state(tokyo_player, in_tokyo=False)
 
     def enter_tokyo(self):
+        self.logger.log(f'\nStep 3: Resolving Tokyo', category='error')
         if any([player.state.in_tokyo for player in self.players]):
+            self.logger.log(f'No change', category='warning')
             return
         self.update_player_state(self.current_player, delta_vp=ENTER_TOKYO_PTS)
         self.update_player_state(self.current_player, in_tokyo=True)
-        if self.verbose:
-            print(f'\n{self.current_player} enters Tokyo ({self.current_player.state})')
+        self.logger.log(f'{self.current_player} enters Tokyo ({self.current_player.state})', category='warning')
 
     def resolve_dice(self, dice):
         self.resolve_victory_point_dice(dice)
         self.resolve_health_dice(dice)
         self.resolve_attack_dice(dice)
-        if self.verbose:
-            print(f'\nStep 2: Resolving dice: {[x.value for x in dice]}')
-            for player in self.players:
-                print(f'{player}: {player.state}')
+        self.logger.log(f'\nStep 2: Resolving dice: {[x.value for x in dice]}', category='error')
+        for player in self.players:
+            self.logger.log(f'{player}: {player.state}', category='info')
 
     def check_winner(self):
         if self.winner_idx != -1:
@@ -128,17 +127,15 @@ class Game:
         elif sum(self.active_players) == 1:
             self.winner_idx = self.active_players.index(True)
 
-        if self.verbose:
-            print(f'Active Players: {self.active_players}')
-            if self.winner_idx != -1:
-                print(f'Winner: {self.players[self.winner_idx]}')
-            else:
-                print('No winner yet.')
+        self.logger.log(f'\nStep 4: Checking Active Players: {self.active_players}', category='error')
+        if self.winner_idx != -1:
+            self.logger.log(f'Winner: {self.players[self.winner_idx]}', category='success')
+        else:
+            self.logger.log('No winner yet.', category='warning')
 
     def step(self):
         if self.active_players[self.current_player_idx]:
-            if self.verbose:
-                print(f'\n\n\nturn {self.turns}: {self.current_player}\'s turn ({self.current_player.state})')
+            self.logger.log(f'\n\nturn {self.turns}: {self.current_player}\'s turn ({self.current_player.state})', category='error')
             self.start_turn()
             dice = self.roll_dice()
             self.resolve_dice(dice)
@@ -154,22 +151,19 @@ if __name__ == '__main__':
     parser.add_argument('--players', '-p', nargs='+', choices=AVAILABLE_AGENTS.keys(), required=True, help='List of players (agent names) to participate in the game.')
     parser.add_argument('--n_games', '-n', type=int, default=100)
     parser.add_argument('--verbose', '-v', action='store_true', help='Print game logs.', default=False)
+    parser.add_argument('--report', '-r', action='store_true', help='Generate game report.', default=False)
     args = parser.parse_args()
 
     assert len(args.players) >= 2, 'At least 2 players are required to play the game.'
+    if args.verbose:
+        assert args.n_games == 1, 'Verbose mode is only supported for single game.'
 
-    winners = [None] * args.n_games
-    turns = [0] * args.n_games
+    logger = GameLogger(player_names=[f"p{p}_{player}" for p, player in enumerate(args.players)], total_games=args.n_games, verbose=args.verbose, report=args.report)
     for i in trange(args.n_games):
         players = [AVAILABLE_AGENTS[player](idx=p, name=player) for p, player in enumerate(args.players)]
-        game = Game(players=players, start_idx=i%len(args.players), verbose=args.verbose)
+        game = Game(players=players, start_idx=i%len(args.players), logger=logger)
+        logger.start_game(game_id=i)
         while game.winner_idx == -1:
             game.step()
-        turns[i] = game.turns
-        winners[i] = str(game.players[game.winner_idx])
-    print('Game Stats:')
-    print(f'------------\nWinners ({args.n_games} games):')
-    for player, count in Counter(winners).items():
-        print(f'{player}: {count} ({count/args.n_games:.2%})')
-    print('------------\nMisc Stats:')
-    print(f'Total turns per player per game: {sum(turns)/args.n_games/len(args.players)}')
+        logger.end_game(winner_name=str(game.players[game.winner_idx]), turn_counts=game.turns)
+    logger.generate_report()
---
+++ report.py
@@ -0,0 +1,196 @@
+from collections import Counter
+import time
+from pathlib import Path
+
+COLORS = {
+    'RESET': '\033[0m',
+    'RED': '\033[91m',
+    'GREEN': '\033[92m',
+    'YELLOW': '\033[93m',
+    'BLUE': '\033[94m',
+    'MAGENTA': '\033[95m',
+    'CYAN': '\033[96m',
+    'BOLD': '\033[1m',
+    'UNDERLINE': '\033[4m',
+    'WHITE': '\033[97m'
+}
+
+CATEGORY_COLORS = {
+    'event': 'WHITE',
+    'success': 'GREEN',
+    'error': 'RED',
+    'warning': 'YELLOW',
+    'info': 'CYAN'
+}
+
+class GameLogger:
+    def __init__(self, player_names, total_games, verbose=False, report=False):
+        self.player_names = player_names
+        self.total_games = total_games
+        self.verbose = verbose
+        self.report = report
+        self.game_logs = []
+        self.current_game_log = None
+        self.winners = []
+        self.turn_counts = []
+
+    def log(self, message, category='event', force_print=False):
+        if self.verbose or force_print:
+            print(COLORS[CATEGORY_COLORS[category]] + message + COLORS['RESET'])
+        if self.report:
+            if len(self.current_game_log['turns']) == 0:
+                self.current_game_log['turns'] = [{'turn_num': 0, 'events': []}]
+            current_turn_events = self.current_game_log['turns'][-1]['events']
+            current_turn_events.append({'message': message, 'category': category})
+    
+    def start_game(self, game_id):
+        if self.report:
+            self.current_game_log = {'game_id': game_id, 'players': self.player_names, 'turns': [], 'winner': None}
+        if self.verbose:
+            self.log(f"\nStarting Game {game_id} with players: {', '.join(self.player_names)}", category='event')
+    
+    def end_game(self, winner_name, turn_counts):
+        if self.report:
+            self.current_game_log['winner'] = winner_name
+            self.current_game_log['turn_counts'] = turn_counts
+            self.game_logs.append(self.current_game_log)
+        if self.verbose:
+            self.log(f"Game ended. Winner: {winner_name} in {turn_counts} turns.", category='info')
+        self.winners.append(winner_name)
+        self.turn_counts.append(turn_counts)
+
+    def generate_report(self):
+        summary_stats = {}
+        summary_stats['winners_count'] = [f'{player}: {count} ({count/self.total_games:.2%})' for player, count in Counter(self.winners).items()]
+        summary_stats['avg_turns_per_player_per_game'] = f"{sum(self.turn_counts)/self.total_games/len(self.player_names):.2f}"
+
+        self.log("\n\n\nGame Stats", category='info', force_print=True)
+        self.log("------------", category='info', force_print=True)
+        self.log(f"Winners ({self.total_games} games):", category='info', force_print=True)
+        for w in summary_stats['winners_count']:
+            self.log(w, category='success', force_print=True)
+        self.log("------------", category='info', force_print=True)
+        self.log("Misc Stats:", category='info', force_print=True)
+        self.log(f"Total turns per player per game: {summary_stats['avg_turns_per_player_per_game']}", category='warning', force_print=True)
+
+        if self.report:
+
+            html_content = f"""
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>godzilla-v2 game report</title>
+                <style>
+                    body {{ font-family: sans-serif; }}
+                    .summary {{ margin-bottom: 20px; padding: 15px; border: 1px solid #ddd; border-radius: 5px; }}
+                    .game-tab {{ border: 1px solid #ddd; margin-bottom: 10px; }}
+                    .tab-header {{ background-color: #f0f0f0; padding: 10px; cursor: pointer; }}
+                    .tab-content {{ padding: 10px; display: none; }}
+                    .tab-content.active {{ display: block; }}
+                    h2 {{ color: #333; }}
+                    h3 {{ color: #555; }}
+                    p {{ margin-bottom: 5px; }}
+                    .log-event strong {{ font-weight: bold; }}
+                    .dice-roll {{ color: blue; }}
+                    .resolve-dice {{ color: orange; }}
+                    .player-state {{ color: green; }}
+                    .enter-tokyo {{ color: magenta; }}
+                    .start-tokyo-turn {{ color: cyan; }}
+                    .game-summary-table {{ width: 100%; border-collapse: collapse; }}
+                    .game-summary-table th, .game-summary-table td {{ border: 1px solid #ddd; padding: 8px; text-align: left; }}
+                    .game-summary-table th {{ background-color: #f0f0f0; }}
+                    .highlight {{ font-weight: bold; color: green; }}
+                    .log-level-event {{ color: black; }}
+                    .log-level-success {{ color: green; }}
+                    .log-level-error {{ color: red; margin-top: 25px; }}
+                    .log-level-warning {{ color: orange; }}
+                    .log-level-info {{ color: blue; }}
+                    
+                </style>
+            </head>
+            <body>
+                <h1>godzilla-v2 game report</h1>
+
+                <div class="summary">
+                    <h2>Game Summary</h2>
+                    <p><strong>Number of Games:</strong> {self.total_games}</p>
+                    <p><strong>Players:</strong> {', '.join(self.player_names)}</p>
+
+                    <h3>Winner Statistics</h3>
+                    <table class="game-summary-table">
+                        <thead>
+                            <tr>
+                                <th>Player</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+            """
+            for w in summary_stats['winners_count']:
+                html_content += f"""
+                            <tr>
+                                <td>{w}</td>
+                            </tr>
+                """
+            html_content += f"""
+                        </tbody>
+                    </table>
+                    <p><strong>Average Turns per Player per Game:</strong> {summary_stats['avg_turns_per_player_per_game']}</p>
+                </div>
+
+                <h2>Game Details</h2>
+            """
+
+            for game_log in self.game_logs:
+                game_id = game_log['game_id']
+                winner = game_log['winner']
+                turn_counts = game_log['turn_counts']
+                players_str = ', '.join(game_log['players'])
+
+                html_content += f"""
+                <div class="game-tab">
+                    <div class="tab-header" onclick="toggleTab('game-{game_id}')">
+                        <h3>Game {game_id + 1}: Winner - {winner}, Turns - {turn_counts}, Players - {players_str}</h3>
+                    </div>
+                    <div id="game-{game_id}" class="tab-content">
+                        <p><strong>Players:</strong> {players_str}</p>
+                        <p><strong>Winner:</strong> <span class="highlight">{winner}</span></p>
+                        <p><strong>Turns:</strong> {turn_counts}</p>
+                """
+                for turn_data in game_log['turns']:
+                    turn_num = turn_data['turn_num']
+                    if turn_num > 0: # Skip turn 0 logs
+                        html_content += f"<h4>Turn {turn_num}</h4>"
+                    for event in turn_data['events']:
+                        level = event['category']
+                        message = event['message']
+
+                        log_message_html = f"<div class='log-event log-level-{level}'>{message}"
+                        log_message_html += "</div>"
+                        html_content += log_message_html
+
+                html_content += """
+                    </div>
+                </div>
+                """
+
+            html_content += """
+            <script>
+                function toggleTab(tabId) {
+                    var content = document.getElementById(tabId);
+                    if (content.style.display === "block") {
+                        content.style.display = "none";
+                    } else {
+                        content.style.display = "block";
+                    }
+                }
+            </script>
+            </body>
+            </html>
+            """
+
+            # report_name = "./reports/" + "_".join(self.player_names) + time.strftime("_%Y%m%d_%H%M%S") + ".html"
+            report_name = "./reports/" + "_".join(self.player_names) + ".html"
+            Path("reports").mkdir(parents=True, exist_ok=True)
+            with open(report_name, 'w') as f:
+                f.write(html_content)
+            print(f"Report written to {report_name}")
